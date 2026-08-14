d27d81cf6a04c5cf1883131fd48e37ccc541a85b
Ray Cooke <rcjcooke@gmail.com>
2026-08-14 09:42:27 +0100

    Formatting

--- Software/src/AOCSPacketStructures.h
+++ Software/src/AOCSPacketStructures.h
@@ -5,13 +5,13 @@
 #include <cstdint>
 
 namespace AOCSPacketConstants {
-constexpr uint8_t kSyncByte0 = 0xAA;
-constexpr uint8_t kSyncByte1 = 0x55;
-constexpr std::size_t kSyncSize = 2;
-constexpr std::size_t kChecksumSize = 2;
-constexpr std::size_t kCommandPayloadSize = 22;
-constexpr std::size_t kTelemetryPayloadSize = 22;
-constexpr std::size_t kFrameSize = kSyncSize + kCommandPayloadSize + kChecksumSize;
+    constexpr uint8_t kSyncByte0 = 0xAA;
+    constexpr uint8_t kSyncByte1 = 0x55;
+    constexpr std::size_t kSyncSize = 2;
+    constexpr std::size_t kChecksumSize = 2;
+    constexpr std::size_t kCommandPayloadSize = 22;
+    constexpr std::size_t kTelemetryPayloadSize = 22;
+    constexpr std::size_t kFrameSize = kSyncSize + kCommandPayloadSize + kChecksumSize;
 }
 
 #pragma pack(push, 1)
